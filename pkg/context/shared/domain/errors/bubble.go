1c21556dc7a4d10fdcc2382903f25a58bb8a7692
Bastean <bastean@pm.me>
2025-03-10 16:40:44 +0000

    refactor(domain): use appendf in the why bubble field

--- pkg/context/shared/domain/errors/bubble.go
+++ pkg/context/shared/domain/errors/bubble.go
@@ -35,7 +35,7 @@ func (b *Bubble) Error() string {
 		why, err := json.Marshal(b.Why)
 
 		if err != nil {
-			why = []byte(fmt.Sprintf("{\"Error\":\"Cannot JSON encoding \"Why\" from error Bubble: [%s]\"}", err))
+			why = fmt.Appendf([]byte{}, "{\"Error\":\"Cannot JSON encoding \"Why\" from error Bubble: [%s]\"}", err)
 		}
 
 		message = fmt.Sprintf("%s: %s", message, why)
